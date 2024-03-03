bf74296a85b8b656134880a23459cf5f24278b43
sj9017 <143730166+sj9017@users.noreply.github.com>
2024-03-03 09:22:17 -0500

    Commit

---
+++ medicine/chatbot.js
@@ -0,0 +1,8 @@
+(function(d, m){
+    var kommunicateSettings = 
+        {"appId":"3c03923ef11be04839e4a4f4abd870272","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    window.kommunicate = m; m._globals = kommunicateSettings;
+})(document, window.kommunicate || {});
